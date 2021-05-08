99918a30237c3f68ad198505fb18a21882dedf81
Chua, Vui Seng <vui.seng.chua@intel.com>
2021-05-08 10:12:06 -0700

    Modify config and cli to get it up and running locally

--- classification/train_val.sh
+++ classification/train_val.sh
@@ -1,6 +1,6 @@
 now=$(date +"%Y%m%d_%H%M%S")
-python -m torch.distributed.launch --nproc_per_node=8 train_imagenet.py \
---config $1 2>&1|tee train-$now.log
+python -m torch.distributed.launch --nproc_per_node=4 train_imagenet.py \
+--config configs/config_resnet50_2by4.yaml 2>&1|tee train-$now.log
 
 
 
